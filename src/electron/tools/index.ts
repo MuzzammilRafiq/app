fd37e6679c25a599e6097de5466dbaff3fab91b8
Muzzammil Rafiq <mzmlrafiqcse@gmail.com>
2025-07-01 23:17:06 +0530

    feat: add date-time utility functions to retrieve current date and time with detailed formatting

---
+++ src/electron/tools/index.ts
@@ -0,0 +1,6 @@
+import { getCurrentDateTime, getCurrentDateTimeFunctionDeclaration } from "./date-time.js";
+
+export default {
+  default: () => "No function found",
+  get_current_date_time: [getCurrentDateTime, getCurrentDateTimeFunctionDeclaration],
+};
