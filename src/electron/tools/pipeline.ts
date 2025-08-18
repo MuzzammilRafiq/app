09d48e7033178c9d7e522c6be35b8ff4a44812a6
Muzzammil Rafiq <mzmlrafiqcse@gmail.com>
2025-08-18 22:53:02 +0530

    reafctor: dont need them

--- src/electron/tools/pipeline.ts
+++
@@ -1,8 +0,0 @@
-import { getDoorResponse } from "./make_plan.js";
-import { router } from "./router.js";
-
-export const pipeline = async (userInput: string) => {
-  const { steps, context } = await getDoorResponse(userInput);
-  const summary = await router(steps, context);
-  return summary;
-};
